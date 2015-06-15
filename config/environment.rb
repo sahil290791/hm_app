6c31820d4ca185029f1e3cb524e6f53b8d2ffbdd
Sahil <sahil.prajapati@tcs.com>
2015-06-15 15:20:25 +0530

    fixing env.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
-app_env_vars = File.join(Rails.root, 'config', 'initializers')
-load(app_env_vars) if File.exists?(app_env_vars)
+#app_env_vars = File.join(Rails.root, 'config', 'initializers')
+#load(app_env_vars) if File.exists?(app_env_vars)
 
 require File.expand_path('../application', __FILE__)
 
